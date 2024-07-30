0f9531de90ae3cf16bf581bc9fa146d01254fe42
Bruno Frazao <bruno.consultoriarj@gmail.com>
2024-07-30 03:33:59 -0300

    Starting to implement Board an Piece

---
+++ src/bordgame/Piece.java
@@ -0,0 +1,28 @@
+package bordgame;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	
+	public Piece() {
+	}
+
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+	
+	
+	
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
+	
+
+}
